4b78328b1ac4c34cbda76dee76cb669506d451f6
HHK <hhk.contact@protonmail.com>
2022-10-27 16:40:30 +0200

    fix: fix ethereum.decode

--- src/functions/furoAutomated.ts
+++ src/functions/furoAutomated.ts
@@ -1,4 +1,4 @@
-import { BigInt, ethereum, log } from '@graphprotocol/graph-ts';
+import { Address, BigInt, Bytes, ethereum } from '@graphprotocol/graph-ts';
 import { CreateFuroAutomated } from '../../generated/FuroAutomatedTimeFactory/FuroAutomatedTimeFactory';
 import { FuroAutomated, FuroAutomatedAmount, FuroAutomatedTime } from '../../generated/schema';
 import { getOrCreateFactory } from './factory';
@@ -11,11 +11,11 @@ function createFuroAutomated(event: CreateFuroAutomated, data: ethereum.Tuple, t
   furoAutomated.type = type;
   furoAutomated.furoId = data[0].toBigInt();
   furoAutomated.owner = event.transaction.from;
-  furoAutomated.token = data[1].toAddress();
-  furoAutomated.vesting = data[4].toBoolean();
-  furoAutomated.withdrawTo = data[2].toAddress();
-  furoAutomated.toBentoBox = data[5].toBoolean();
-  furoAutomated.taskData = data[6].toBytes();
+  furoAutomated.token = Address.fromString(data[1].toBigInt().toHex());
+  furoAutomated.vesting = data[4].toBigInt().isZero();
+  furoAutomated.withdrawTo = Address.fromString(data[2].toBigInt().toHex());
+  furoAutomated.toBentoBox = data[5].toBigInt().isZero();
+  furoAutomated.taskData = Bytes.fromHexString(data[6].toBigInt().toHex());
   furoAutomated.balance = BigInt.fromU32(0);
   furoAutomated.taskId = event.params.taskId;
   furoAutomated.active = true;
@@ -28,7 +28,6 @@ export function createFuroAutomatedTime(event: CreateFuroAutomated): FuroAutomat
   const data = ethereum.decode('(uint256, address, address, uint32, bool, bool, bytes)', event.params.data);
   if (!data) throw 'Unable to decode data.';
   const decodedData = data.toTuple();
-  log.debug('decoded: {}', [decodedData.join(',')]);
 
   let furoAutomatedTime = new FuroAutomatedTime(event.params.clone.toHex());
   furoAutomatedTime.furoAutomated = createFuroAutomated(event, decodedData, 'TIME').id;
